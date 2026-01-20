45e749e4fc3ebb5a7245a7c467007f882cb6db1b
safiya <bioelatrash@gmail.com>
2026-01-20 14:27:05 -0100

    setup basic rails 8 auth with generator

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }, on: :create
+end
